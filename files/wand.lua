ce490180c2b81ac4c5a54ceef9b1a31e7cc54d13
Kaedenn <kaedenn@gmail.com>
2024-11-10 10:21:51 -0900

    Add extra work

---
+++ files/wand.lua
@@ -0,0 +1,43 @@
+--[[ Spawn the Bad Apple!! wand ]]
+
+dofile_once("mods/badapple/files/lib/EZWand.lua")
+dofile_once("mods/badapple/files/utility.lua")
+
+---Spawns a shiny new Bad Apple!! wand
+---@param wand_x number
+---@param wand_y number
+function spawn_badapple_wand(wand_x, wand_y)
+    local wand = EZWand({
+        shuffle = false,
+        spellsPerCast = 1,
+        castDelay = 20,
+        rechargeTime = 40,
+        manaMax = 100,
+        mana = 100,
+        manaChargeSpeed = 50,
+        capacity = 1,
+        spread = 0,
+        speedMultiplier = 1,
+    }, wand_x, wand_y)
+    wand:AttachSpells("BAD_APPLE")
+    wand:SetSprite("mods/badapple/files/wands/scepter_01.xml", 3, 4, 17, 5)
+    wand:SetName("$item_wand_badapple", true)
+    wand:PlaceAt(wand_x, wand_y)
+end
+
+---Spawn just the Bad Apple!! spell
+---@param xpos number
+---@param ypos number
+function spawn_spell(xpos, ypos)
+    if xpos == nil or ypos == nil then
+        local player = get_players()[1]
+        if not player then return end
+        local px, py = EntityGetTransform(player)
+        if not px or not py then return end
+        if xpos == nil then xpos = px end
+        if ypos == nil then ypos = py end
+    end
+    CreateItemActionEntity("BAD_APPLE", xpos, ypos)
+end
+
+-- vim: set ts=4 sts=4 sw=4 et:
